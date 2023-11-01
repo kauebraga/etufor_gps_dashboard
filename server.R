9d8a8f2ba8a1c16395c46a55799309c5bbb59664
Kaue Braga <kauebraga90@gmail.com>
2023-10-31 22:20:35 -0300

    update

--- server.R
+++ server.R
@@ -7,9 +7,30 @@ segments_variables <- setDT(readRDS("data/gps_by_segment_variables.rds"))
 stops_routes <- readRDS("data/stops_gtfs_routes_sf.rds")
 stops_unique <- readRDS("data/stops_gtfs_sf.rds")
 
+# graphs
+data_month_all <- readRDS("data/graphs/graphs_month_all.rds")
+data_month <- readRDS("data/graphs/graphs_month.rds")
 
-server <- function(input, output) {
+data_interval_all <- readRDS("data/graphs/graphs_interval_all.rds")
+data_interval <- readRDS("data/graphs/graphs_interval.rds")
+
+data_fluxo_all <- readRDS("data/graphs/graphs_fluxo_all.rds")
+
+server <- function(input, output, session) {
   
+  
+  observeEvent(c(input$submit), {
+    
+    req(input$submit >= 1)
+    
+    print("guaaaa")
+    
+    shinyjs::disable(id = "download_png_bttn")
+    shinyjs::disable(id = "download_html_bttn")
+    shinyjs::disable(id = "download_gpkg_data")
+    
+    
+  })
   
   # set data
   data <- reactiveValues(start = NULL,
@@ -302,6 +323,299 @@ server <- function(input, output) {
     }
     
   )  
+  
+  observeEvent(c(input$interval, input$route), {
+    
+    
+    
+    
+  })
+  
+
+# graphs --------------------------------------------------------------------------------------
+  
+  
+  output$output_graph_month <- renderHighchart({
+    
+    # graphs
+    # data_month_all <- readRDS("data/graphs/graphs_month_all.rds")
+    # data_month <- readRDS("data/graphs/graphs_month.rds")
+    
+    highchart() %>%
+      
+      hc_add_series(data = data_month_all,
+                    hcaes(x = month, y = velocidade),
+                    type = "line",
+                    color = "#F7B93B",
+                    # lineWidth = 5,
+                    # opacity = 0.5,
+                    name = "Todas",
+                    tooltip = list(pointFormat = sprintf("{series.name}: {point.y} km/h"),
+                                   valueDecimals = 1)
+      ) %>%
+      
+      hc_title(text = "Velocidade media por mes") %>%
+      
+      # hc_legend(verticalAlign = "top") %>%
+      hc_yAxis(title = list(text = "Velocidade (km/h)", style = list(fontSize = 16)),
+               labels = list(style = list(fontSize = 15)),
+               maxPadding = 0.001) %>%
+      hc_xAxis(title = list(text = "", style = list(fontSize = 16)),
+               labels = list(style = list(fontSize = 15)),
+               type = "category")
+      # hc_add_theme(hc_theme_smpl(
+      #   # chart = list(
+      #   #   # backgroundColor = "#1C1C1C",
+      #   #   # backgroundColor = "white",
+      #   #   style = list(fontFamily = "Franklin Gothic Book"))
+      #   title = list(style = list(fontFamily = "Franklin Gothic Demi",
+      #                             textTransform = "none"))
+      #   
+      # ))
+    
+    
+    
+    
+  })
+  
+  
+  
+
+  output$output_graph_interval <- renderHighchart({
+    
+    # graphs
+    # data_month_all <- readRDS("data/graphs/graphs_month_all.rds")
+    # data_month <- readRDS("data/graphs/graphs_month.rds")
+    
+    highchart() %>%
+      
+      hc_add_series(data = data_interval_all,
+                    hcaes(x = interval, y = velocidade),
+                    type = "line",
+                    color = "#F7B93B",
+                    # lineWidth = 5,
+                    # opacity = 0.5,
+                    name = "Todas",
+                    tooltip = list(pointFormat = sprintf("{series.name}: {point.y} km/h"),
+                                   valueDecimals = 1)
+      ) %>%
+      
+      hc_title(text = "Velocidade media por intervalo de 15 minutos") %>%
+      
+      # hc_legend(verticalAlign = "top") %>%
+      hc_yAxis(title = list(text = "Velocidade (km/h)", style = list(fontSize = 16)),
+               labels = list(style = list(fontSize = 15)),
+               maxPadding = 0.001) %>%
+      hc_xAxis(title = list(text = "", style = list(fontSize = 16)),
+               labels = list(style = list(fontSize = 15)),
+               type = "category")
+      # hc_add_theme(hc_theme_smpl(
+      #   # chart = list(
+      #   #   # backgroundColor = "#1C1C1C",
+      #   #   # backgroundColor = "white",
+      #   #              style = list(fontFamily = "Franklin Gothic Book"))
+      #   
+      #   title = list(style = list(fontFamily = "Franklin Gothic Demi",
+      #                             textTransform = "none"))
+      #   
+      # ))
+    
+    
+    
+    
+  })
+
+  output$output_graph_fluxo <- renderHighchart({
+    
+    # graphs
+    # data_month_all <- readRDS("data/graphs/graphs_month_all.rds")
+    # data_month <- readRDS("data/graphs/graphs_month.rds")
+    
+    highchart() %>%
+      
+      hc_add_series(data = data_fluxo_all,
+                    hcaes(x = fluxo_horario, y = velocidade),
+                    type = "line",
+                    color = "#F7B93B",
+                    # lineWidth = 5,
+                    # opacity = 0.5,
+                    name = "Todas",
+                    tooltip = list(pointFormat = sprintf("{series.name}: {point.y} km/h"),
+                                   valueDecimals = 1)
+      ) %>%
+      
+      hc_title(text = "Velocidade media por fluxo horario") %>%
+      
+      # hc_legend(verticalAlign = "top") %>%
+      hc_yAxis(title = list(text = "Velocidade (km/h)", style = list(fontSize = 16)),
+               labels = list(style = list(fontSize = 15)),
+               maxPadding = 0.001) %>%
+      hc_xAxis(title = list(text = "[veh/hora]", style = list(fontSize = 16)),
+               labels = list(style = list(fontSize = 15)),
+               type = "category")
+      # hc_add_theme(hc_theme_smpl(
+      #   # chart = list(
+      #   #   # backgroundColor = "#1C1C1C",
+      #   #   # backgroundColor = "white",
+      #   #              style = list(fontFamily = "Franklin Gothic Book"))
+      #   
+      #   title = list(style = list(fontFamily = "Franklin Gothic Demi",
+      #                             textTransform = "none"))
+      #   
+      # ))
+    
+    
+    
+    
+  })
+  
+  
+  reV_order <- reactiveValues(values_route = NULL, values_max = NULL,
+                              values_interval = NULL)
+  
+  # use reactive to get and sort the selected terms in the order of selection
+  ordered_colnames_route <- reactive({
+    
+    # req(input$city_compare1_initial)
+    # print("que1")
+    # print(input$city_compare1_initial)
+    
+    if (length(reV_order$values_route) > length(input$route1)) {
+      reV_order$values_route <- reV_order$values_route[reV_order$values_route %in% input$route1]
+      # print("que1")
+      
+    }else {
+      reV_order$values_route <- c(reV_order$values_route, input$route1[!input$route1 %in% reV_order$values_route])
+      # print("aqui")
+      # print(reV_order$values)
+    }
+    reV_order$values_route
+  })
+  ordered_colnames_interval <- reactive({
+    
+    # req(input$city_compare1_initial)
+    # print("que1")
+    # print(input$city_compare1_initial)
+    
+    if (length(reV_order$values_interval) > length(input$interval1)) {
+      reV_order$values_interval <- reV_order$values_interval[reV_order$values_interval %in% input$interval1]
+      # print("que1")
+      
+    }else {
+      reV_order$values_interval <- c(reV_order$values_interval, input$interval1[!input$interval1 %in% reV_order$values_interval])
+      # print("aqui")
+      # print(reV_order$values)
+    }
+    reV_order$values_interval
+  })
+  
+  
+  
+  observe({ ordered_colnames_route() }) # use an observe to update the reactive function above
+  observe({ ordered_colnames_interval() }) # use an observe to update the reactive function above
+  
+  
+  observeEvent(c(input$reset_graph1), {
+    
+    req(input$reset_graph1 >= 1)
+    
+    highchartProxy("output_graph_interval") %>%
+      hcpxy_remove_series(id = c(ordered_colnames_route(), ordered_colnames_interval()))
+    
+    highchartProxy("output_graph_month") %>%
+      hcpxy_remove_series(id = c(ordered_colnames_route(), ordered_colnames_interval()))
+    
+    updatePickerInput(
+      session = session,
+      inputId = "route1",
+      selected = character(0))
+    
+    updatePickerInput(
+      session = session,
+      inputId = "interval1",
+      selected = character(0))
+    
+    
+    
+  })
+  
+  observeEvent(c(input$route1), {
+    
+    # print(input$route)
+    
+    # req(input$submit >= 1)
+    
+    # print(tail(ordered_colnames(), 1))
+    
+    data1 <- data_interval[route_id == tail(ordered_colnames_route(), 1)]
+    data1 <- data1[, .(velocidade = round(median(velocidade), 1)), by = .(route_id, interval)]
+    setorder(data1, route_id, interval)
+    
+    
+    highchartProxy("output_graph_interval") %>%
+      # hcpxy_remove_series(id = "que") %>%
+      hcpxy_add_series(data = data1, hcaes(x = interval, y = velocidade),
+                       id = tail(ordered_colnames_route(), 1),
+                       type = "line",
+                       color = "red",
+                       name = tail(ordered_colnames_route(), 1),
+                       tooltip = list(pointFormat = sprintf("{series.name}: {point.y} km/h"),
+                                      valueDecimals = 1)
+                       
+      )
+    
+    
+    data2 <- data_month[route_id == tail(ordered_colnames_route(), 1)]
+    data2 <- data2[, .(velocidade = round(median(velocidade), 1)), by = .(month)]
+    setorder(data2, month)
+    
+    print(data1)
+    
+    highchartProxy("output_graph_month") %>%
+      # hcpxy_remove_series(id = "que") %>%
+      hcpxy_add_series(data = data2, hcaes(x = month, y = velocidade),
+                       id = tail(ordered_colnames_route(), 1),
+                       type = "line",
+                       color = "red",
+                       name = tail(ordered_colnames_route(), 1),
+                       tooltip = list(pointFormat = sprintf("{series.name}: {point.y} km/h"),
+                                      valueDecimals = 1)
+                       
+      )
+    
+    
+  })
+  
+  observeEvent(c(input$interval1), {
+    
+    # print(input$route)
+    
+    # req(input$submit >= 1)
+    
+    # print(tail(ordered_colnames(), 1))
+    
+      
+    data1 <- data_month[interval == tail(ordered_colnames_interval(), 1)]
+      
+    data1 <- data1[, .(velocidade = round(median(velocidade), 1)), by = .(month)]
+    setorder(data1, month)
+    
+    
+    highchartProxy("output_graph_month") %>%
+      # hcpxy_remove_series(id = "que") %>%
+      hcpxy_add_series(data = data1, hcaes(x = month, y = velocidade),
+                       id = tail(ordered_colnames_interval(), 1),
+                       type = "line",
+                       color = "red",
+                       name = tail(ordered_colnames_interval(), 1),
+                       tooltip = list(pointFormat = sprintf("{series.name}: {point.y} km/h"),
+                                      valueDecimals = 1)
+                       
+      )
+    
+    
+  })
+  
   
   
   }
--- ui.R
+++ ui.R
@@ -3,77 +3,135 @@ routes_shapes_list <- readRDS("data/routes_shapes_list.rds")
 intervals_list <- readRDS("data/intervals.rds")
 
 
-page_sidebar(
+page_navbar(
+  shinyjs::useShinyjs(),
   title = "Monitoramento de velocidade ETUFOR",
-  sidebar = sidebar(
-    title = "Filtros",
-    width = 325, 
-    # bg = "#e3e4e6",
-    
-    # selecionar intervalo temporal
-    pickerInput(inputId = "interval",
-                label = "Intervalos(s)",
-                choices = c(intervals_list),
-                multiple = TRUE,
-                options = pickerOptions(selectAllText = "Selecionar todos",
-                                        deselectAllText = "Zerar selecao",
-                                        size = 10,
-                                        actionsBox = TRUE,
-                                        noneSelectedText = "Filtre para intervalo(s)",
-                                        style = "background: white"
-                )
-    ),
-    
-    # selecionar rota
-    pickerInput(inputId = "route",
-                label = "Linha(s)",
-                choices = c(unique(routes_shapes_list$route_id)),
-                multiple = TRUE,
-                options = pickerOptions(selectAllText = "Selecionar todas",
-                                        deselectAllText = "Zerar selecao",
-                                        size = 10,
-                                        actionsBox = TRUE,
-                                        liveSearch = TRUE,
-                                        noneSelectedText = "Filtre para linha(s)",
-                                        style = "background: white"
-                )
+  theme = bs_theme(preset = "litera"),
+  id = "navbar",
+  bg = "#F7B93B",
+  nav_panel(title = "Mapa",
+            layout_sidebar(
+              sidebar = sidebar(
+                title = "Filtros",
+                width = 325, 
+                # bg = "#e3e4e6",
                 
+                # selecionar intervalo temporal
+                pickerInput(inputId = "interval",
+                            label = "Intervalos(s)",
+                            choices = c(intervals_list),
+                            multiple = TRUE,
+                            options = pickerOptions(selectAllText = "Selecionar todos",
+                                                    deselectAllText = "Zerar selecao",
+                                                    size = 10,
+                                                    actionsBox = TRUE,
+                                                    noneSelectedText = "Filtre para intervalo(s)",
+                                                    style = "background: white"
+                            )
+                ),
                 
-    ),
-    
-    
-    numericInput(inputId = "velocidade_maxima", 
-                 label = "Selecione uma velocidade maxima (km/h)", 
-                 value = 40,
-                 max = 60,
-                 min = 0
-    ),
-    actionButton(inputId = "submit",
-                 label = "Filtrar"),
-    hr(),
-    downloadBttn(outputId = "download_png",
-                 label = "Download map (.png)",
-                 style = "simple",
-                 size = "sm"
-    ),
-    downloadBttn(outputId = "download_html",
-                 label = "Download map (.html)",
-                 style = "simple",
-                 size = "sm"
-    ),
-    downloadBttn(outputId = "download_data",
-                 label = "Download data (.gpkg)",
-                 style = "simple",
-                 size = "sm"
+                # selecionar rota
+                pickerInput(inputId = "route",
+                            label = "Linha(s)",
+                            choices = c(unique(routes_shapes_list$route_id)),
+                            multiple = TRUE,
+                            options = pickerOptions(selectAllText = "Selecionar todas",
+                                                    deselectAllText = "Zerar selecao",
+                                                    size = 10,
+                                                    actionsBox = TRUE,
+                                                    liveSearch = TRUE,
+                                                    noneSelectedText = "Filtre para linha(s)",
+                                                    style = "background: white"
+                            )
+                            
+                            
+                ),
+                
+                numericInput(inputId = "velocidade_maxima", 
+                             label = "Selecione uma velocidade maxima (km/h)", 
+                             value = 40,
+                             max = 60,
+                             min = 0
+                ),
+                actionButton(inputId = "submit",
+                             label = "Filtrar"),
+                hr(),
+                downloadBttn(outputId = "download_png",
+                             label = "Download map (.png)",
+                             style = "simple",
+                             size = "sm"
+                ),
+                downloadBttn(outputId = "download_html",
+                             label = "Download map (.html)",
+                             style = "simple",
+                             size = "sm"
+                ),
+                downloadBttn(outputId = "download_data",
+                             label = "Download data (.gpkg)",
+                             style = "simple",
+                             size = "sm"
+                )
+                # downloadBttn(outputId = "download_html",
+                #              label = "Download",
+                #              style = "simple"
+                # )
+                
+              ),
+              "Mapa",
+              leafletOutput("map", height = 600),
+            )),
+  nav_panel(
+    "Gráficos",
+    value = "graphs",
+    layout_sidebar(
+      sidebar = sidebar(
+        title = "Filtros",
+        width = 325, 
+        # bg = "#e3e4e6",
+        
+        # selecionar intervalo temporal
+        pickerInput(inputId = "interval1",
+                    label = "Intervalos(s)",
+                    choices = c(intervals_list),
+                    multiple = TRUE,
+                    options = pickerOptions(selectAllText = "Selecionar todos",
+                                            deselectAllText = "Zerar selecao",
+                                            size = 10,
+                                            actionsBox = TRUE,
+                                            noneSelectedText = "Filtre para intervalo(s)",
+                                            style = "background: white"
+                    )
+        ),
+        
+        # selecionar rota
+        pickerInput(inputId = "route1",
+                    label = "Linha(s)",
+                    choices = c(unique(routes_shapes_list$route_id)),
+                    multiple = TRUE,
+                    options = pickerOptions(selectAllText = "Selecionar todas",
+                                            deselectAllText = "Zerar selecao",
+                                            size = 10,
+                                            actionsBox = TRUE,
+                                            liveSearch = TRUE,
+                                            noneSelectedText = "Filtre para linha(s)",
+                                            style = "background: white"
+                    )
+                    
+                    
+        ),
+        actionButton("reset_graph1", "Resetar gráficos"),
+      ),
+      # leafletOutput("map")
+      fluidRow(
+               column(width = 6,
+                      highchartOutput("output_graph_month", width = 400, height = 280)),
+               column(width = 6,
+                      highchartOutput("output_graph_fluxo", width = 400, height = 280))),
+      fluidRow(      
+               column(width = 12,
+                      highchartOutput("output_graph_interval", width = 800, height = 350)))
+      
+      
     )
-    # downloadBttn(outputId = "download_html",
-    #              label = "Download",
-    #              style = "simple"
-    # )
-    
-  ),
-  card(
-    card_header("Map"),
-    leafletOutput("map")
   )
 )
